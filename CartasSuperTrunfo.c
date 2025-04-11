9cfcb861156e873374feb83a30ea499d2ab4c324
iranrj <iranlisboa16@gmail.com>
2025-04-11 20:04:24 +0000

    criei uns numeros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,7 +14,11 @@ int main(){
 	float areakm1;
 	float PIB1;
 	int pontotu1;
-	//Carta N1 Pegando informaçoes
+	float calculoDP;
+	float calculoPP;
+    float calculoDP1;
+	float calculoPP1;
+	//Carta N1
 	printf("Carta 1:\n");
 	
 	printf("Estado - Uma letra de 'A' a 'H': ");
@@ -37,9 +41,12 @@ int main(){
 	//printf("O PIB e : %.3f", PIB);
 	printf("Informe o numero de Pontos Tuisticos: ");
 	scanf("%d", &pontotu);
+	calculoDP = populacao / areakm;
+	calculoPP = PIB / populacao;
+	
 	//printf("Tem %d pontos turisticos.", pontotu);
 	
-	//CARTA N2 Pegando informaçoes
+	//CARTA N2
 	printf("Carta 2:\n");
 	
 	printf("Estado - Uma letra de 'A' a 'H': ");
@@ -62,11 +69,17 @@ int main(){
 
 	printf("Informe o numero de Pontos Tuisticos: ");
 	scanf("%d", &pontotu1);
+	calculoDP1 = populacao1 / areakm1;
+	calculoPP1 = PIB1 / populacao1;
 	//Mostrar resultados
-	printf("Carta 1:\nEstado: %c\ncodigo: %c%s\nNome da Cidade: %s\nPopulação: %d\nArea: %.3f\nPIB: %.3f\nNumero de Pontos Turisticos: %d\n", estado, estado, codigoC, Ncidade, populacao, areakm, PIB, pontotu);
+	printf("Carta 1:\nEstado: %c\ncodigo: %c%s\nNome da Cidade: %s\nPopulação: %d\nArea: %.3f\nPIB: %.3f\nNumero de Pontos Turisticos: %d\n", estado, estado, codigoC , Ncidade, populacao, areakm, PIB, pontotu);
 	printf("--\n");
-        printf("Carta 2:\nEstado: %c\ncodigo: %c%s\nNome da Cidade: %s\nPopulação: %d\nArea: %.3f\nPIB: %.3f\nNumero de Pontos Turisticos: %d", estado1, estado1, codigoC1, Ncidade1, populacao1, areakm1, PIB1, pontotu1);
+    printf("Carta 2:\nEstado: %c\ncodigo: %c%s\nNome da Cidade: %s\nPopulação: %d\nArea: %.3f\nPIB: %.3f\nNumero de Pontos Turisticos: %d\n", estado1, estado1, codigoC1, Ncidade1, populacao1, areakm1, PIB1, pontotu1);
+	printf("Carta 1 a Densidade Populacional e: %.2f hab/km²\n", calculoDP);
+	printf("Carta 2 a Densidade Populacional e: %.2f hab/km²\n", calculoDP1);
+	printf("Carta 1 PIB per Capita: %.2f reais\n", calculoPP);
+	printf("Carta 2 PIB per Capita: %.2f reais", calculoPP1);
 	
-        return 0;
+	return 0;
 	
 }
